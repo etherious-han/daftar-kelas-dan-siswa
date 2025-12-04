c771cfa4ebbb1ee345230946b497b22bb254191b
Han <dehanbbackup@gmail.com>
2025-12-04 14:42:05 +0700

    Update CRUD Laravel project

--- database/migrations/2025_12_02_081158_create_siswa_table.php
+++ database/migrations/2025_12_02_081158_create_siswa_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->integer('nis');
             $table->string('no_hp');
             $table->string('jenis_kelamin');
-            
             $table->timestamps();
         });
     }
